7f244c1c7980d40649e46643fa5bf81d05ee5367
健太郎 <kentarou@kentarou-no-MacBook-Air.local>
2020-09-14 21:25:01 +0900

    included canvas in DOM, but scene was black

--- js/three-vue.js
+++ js/three-vue.js
@@ -3,44 +3,34 @@ window.onload = function(){
 	new Vue({
 			el:"#app",
 			data: {
-				windowWidth: 0,
-				windowHeight: 0,
 				scene : new THREE.Scene(),
-				renderer : new THREE.WebGLRenderer(),
-				camera : new THREE.PerspectiveCamera(45,300,200),
-				light : new THREE.AmbientLight(0xffffff,1.0),
+				renderer : new THREE.WebGLRenderer({antialias: true}),
+				camera :  new THREE.PerspectiveCamera(30,1,1,1000),
+				light : new THREE.DirectionalLight(0xFFFFFF, 1),
 				geometry : new THREE.BoxGeometry(400, 400, 400),
 				material : new THREE.MeshNormalMaterial(),
-				cube : new THREE.Mesh(this.geometry,this.material),
+				cube : new THREE.Mesh(this.geometry,this.material)
 			},
 			methods:{
-				getWindowWidth: function( e ) {
-					this.windowWidth = document.documentElement.clientWidth;
-				},
-				getWindowHeight: function( e ) {
-					this.windowHeight = document.documentElement.clientHeight;
-				},
 				animate(){
 					this.renderer.render(this.scene, this.camera);
 					requestAnimationFrame(this.animate);
-					//console.log('acting');
+					//console.log();
 				}
 			},
-			created(){
+			mounted() {
+				let canvas = document.getElementById('myCanvas');
+				canvas.appendChild(this.renderer.domElement);
+				this.renderer.setPixelRatio(window.devicePixelRatio);
+				this.renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+				this.camera.aspect = canvas.clientWidth / canvas.clientHeight;
 				this.scene.add(this.camera);
 				this.scene.add(this.light);
 				this.scene.add(this.cube);
-			},
-			beforeMount() {
-				window.addEventListener("resize", this.getWindowWidth );
-				window.addEventListener("resize", this.getWindowHeight );
-			},
-			mounted: function(){
+				//console.log('mounted acting');
 
-			},
-			beforeDestroy() {
-				window.removeEventListener("resize", this.getWindowWidth );
-				window.removeEventListener("resize", this.getWindowHeight );
+				this.animate();
+
 			}
 
 	});
